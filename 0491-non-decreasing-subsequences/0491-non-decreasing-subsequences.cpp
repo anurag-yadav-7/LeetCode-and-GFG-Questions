cd19659a165ae61de629ec7cc5cfb10194fbd987
Anurag Yadav <92670570+white-wolf-7@users.noreply.github.com>
2023-01-20 23:17:58 +0530

    Time: 89 ms (59.11%), Space: 25.6 MB (50.90%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,50 +1,51 @@
-class Solution {
-    
-    set<vector<int>> res;
+class Solution
+{
+
+    set<vector < int>> res;
     vector<int> temp;
-    
-public:
-    
-    void rec(int ind,vector<int> &nums)
-    {
-        // cout<<"ind: "<<ind<<endl;
-        if(ind==nums.size())
+
+    public:
+
+        void rec(int ind, vector<int> &nums)
         {
-            // cout<<"Base case..."<<endl;
-            if(temp.size()>1)
-            res.insert(temp);
-            return;
-        }
-     
-        if(nums[ind]>=temp.back())
+           	// cout<<"ind: "<<ind<<endl;
+            if (ind == nums.size())
             {
-                // cout<<"greater than top... "<<endl;
+               	// cout<<"Base case..."<<endl;
+                if (temp.size() > 1)
+                    res.insert(temp);
+                return;
+            }
+
+            
+            if (nums[ind] >= temp.back())
+            {
+               	// cout<<"greater than top... "<<endl;
                 temp.push_back(nums[ind]);
-                rec(ind+1,nums);
+                rec(ind + 1, nums);
                 temp.pop_back();
-                rec(ind+1,nums);
+                rec(ind + 1, nums);
             }
             else
             {
-                // cout<<"case2: not greater than top..."<<endl;
-                rec(ind+1,nums);
+               	// cout<<"case2: not greater than top..."<<endl;
+                rec(ind + 1, nums);
             }
-    }
-    
-    
-    vector<vector<int>> findSubsequences(vector<int>& nums) {
-        
-        for(int i=0;i<nums.size();i++)
+        }
+
+    vector<vector < int>> findSubsequences(vector<int> &nums)
+    {
+
+        for (int i = 0; i < nums.size(); i++)
         {
-            // cout<<"Starting from: "<<nums[i]<<endl;
-            temp.push_back(nums[i]);    
-            rec(i+1,nums);
+           	// cout<<"Starting from: "<<nums[i]<<endl;
+            temp.push_back(nums[i]);
+            rec(i + 1, nums);
             temp.pop_back();
         }
-        vector<vector<int>> ans;
-        for(auto it: res)
+        vector<vector < int>> ans;
+        for (auto it: res)
             ans.push_back(it);
         return ans;
-        
     }
 };
