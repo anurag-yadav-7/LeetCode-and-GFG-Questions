ded793ec8a5f90759712236fab8c5e9fc56af325
Anurag Yadav <92670570+white-wolf-7@users.noreply.github.com>
2023-01-30 12:52:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (6.99%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int> vec(38,0);
-        vec[0]=0;
-        vec[1]=1;
-        vec[2]=1;
+        vector<int> vec(n+1,0);
+        if(n>=2)
+            vec[2]=1;
+        if(n>=1)
+            vec[1]=1;
+        if(n>=0)
+            vec[0]=0;
         
         // if(n==0) //|| n==1 || n==2)
         //     return vec[n];
