055443b1060d657eeaa1a2bdae56cb334a0afdb2
Anurag Yadav <92670570+white-wolf-7@users.noreply.github.com>
2023-03-16 00:59:42 +0530

    Time: 11 ms (10.92%), Space: 10.5 MB (59.30%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -22,13 +22,18 @@ public:
             int n=q.size();
             // cout<<"new level: ";
             TreeNode* temp= new TreeNode(-1);
-            vector<int> checker;
             
             while(n--)
             {
                 TreeNode *curr=q.front();
                 q.pop();
-                checker.push_back(curr->val);
+                
+                if(flag== false &&  curr->val==-1)
+                    flag=true;
+                    
+                else if(flag== true && curr->val!=-1)
+                    return false;
+                
                 if(curr->val!=-1)
                 {
                     if(curr->left)
@@ -39,24 +44,7 @@ public:
                         q.push(curr->right);
                     else
                         q.push(temp);
-                }
-                
-                // cout<<curr->val<<" ";
-                
-            }
-            // cout<<endl;
-            int size = checker.size();
-            
-            for(int i = 0 ; i< size; i++)
-            {
-                if(flag==false &&  checker[i]==-1)
-                {
-                    flag=true;
-                }
-                else if(flag==true && checker[i]!=-1)
-                {
-                    return false;
-                }
+                }   
             }
         }
         return true;
