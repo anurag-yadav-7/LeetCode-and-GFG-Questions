e626689fbc0c92857e5627b7df31bff377708832
Anurag Yadav <92670570+white-wolf-7@users.noreply.github.com>
2023-07-07 08:24:13 +0530

    Time: 20 ms (97.97%), Space: 22.2 MB (78.56%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,34 +2,45 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         
-        int n=nums.size();
-        int low=0,high=n-1;
-        int mid;
+        int n = nums.size();
         
-        while(low<=high)
+        if(n == 1)
+            return nums[0];
+        
+        if(nums[0] != nums[1])
+            return nums[0];
+        if(nums[n-1]!= nums[n-2] )
+            return nums[n-1];
+        
+        int low = 1, high = n - 2 ;
+        int mid, res;
+        
+        
+        while( low <= high )
         {
-            mid=(low+high)/2;
+            mid = low + (high - low ) / 2;
             
-            if(mid%2==0)
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
+               return nums[mid];
+            
+            if(nums[mid] == nums[mid-1])
             {
-                if(mid+1<n && nums[mid+1]==nums[mid])
-                    low=mid+2;
-                else if(mid-1>=0 && nums[mid-1]==nums[mid])
-                    high=mid-2;
+                if(mid % 2 == 0)
+                    high = mid - 1;
                 else
-                    return nums[mid];
+                    low = mid + 1;
             }
-            else
+            else if(nums[mid] == nums[mid+1])
             {
-                if(mid+1<n && nums[mid+1]==nums[mid])
-                    high=mid-1;
-                else if(mid-1>=0 && nums[mid-1]==nums[mid])
-                    low=mid+1;
+                if(mid % 2 == 0)
+                    low = mid + 1;
                 else
-                    return nums[mid];    
+                    high = mid - 1;
             }
         }
-        return nums[low];
+        
+        return -1;
+        
         
     }
 };
