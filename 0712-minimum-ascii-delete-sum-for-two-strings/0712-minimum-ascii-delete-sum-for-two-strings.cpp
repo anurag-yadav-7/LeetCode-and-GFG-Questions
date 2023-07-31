73c060c083db05bcc8f267d9b6e1a908cbdb39e2
Anurag Yadav <92670570+anurag-yadav-7@users.noreply.github.com>
2023-08-01 01:47:56 +0530

    Time: 47 ms (66.19%), Space: 15.2 MB (40.90%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,46 +1,44 @@
 class Solution {
-    int temp = 0;
-    int rec(int i , int j, string &s1, string &s2, vector<vector<int>> &dp)
-    {
-        if(dp[i+1][j+1] != -1)
-            return dp[i+1][j+1];
-        if(i<0){
-            if(j<0){
+    int rec(int n1, int n2, string &s1, string &s2, vector<vector<int>> &dp){
+        
+        if(dp[n1+1][n2+1] != -1) return dp[n1+1][n2+1];
+
+        if(n1<0 || n2<0){
+            int temp = 0;
+            if(n1<0 && n2>=0){
+                int index = n2;
+                while(n2>=0){
+                    temp += s2[n2];
+                    n2--;
+                }
+                // cout<<"pehla if"<<endl;
+                return dp[n1+1][index+1] = temp;
+            }
+            else if(n2<0 && n1>=0){
+                int index = n1;
+                while(n1>=0){
+                    temp += s1[n1];
+                    n1--;
+                }
+                // cout<<"dusra if"<<endl;
+                return dp[index+1][n2+1] = temp;
+            }
+            else
+            {
+                // cout<<"last if 0 wala"<<endl;
                 return 0;
             }
-            int index = j;
-            temp = 0;
-            while(j>=0){
-                temp += s2[j];
-                j--;
-            }
-            return dp[i+1][index+1] = temp;
         }
-        if(j<0){
-            int index = i;
-            temp = 0;
-            while(i>=0){
-                temp += s1[i];
-                i--;
-            }
-            return dp[index+1][j+1] = temp;
-        }
-        
-        if(s1[i] == s2[j])
-            return dp[i+1][j+1] = rec(i-1, j-1, s1, s2,dp);
-        
-        int first = s1[i] + rec(i-1, j, s1, s2 ,dp);
-        int second = s2[j] + rec(i, j-1, s1, s2 ,dp);
-        
-        return dp[i+1][j+1] = min(first, second);
+        if(s1[n1] == s2[n2]) 
+            return dp[n1+1][n2+1] = rec(n1-1,n2-1,s1,s2,dp);
+        return dp[n1+1][n2+1] = min((s1[n1]+rec(n1-1,n2,s1,s2,dp)),(s2[n2]+rec(n1,n2-1,s1,s2,dp)));
     }
     
-public:
+    public:
+
     int minimumDeleteSum(string s1, string s2) {
-        
-        int len1 = s1.length();
-        int len2 = s2.length();
-        vector<vector<int>> dp(len1+1, vector<int> (len2+1,-1));
-        return rec(len1-1, len2-1, s1, s2, dp );
+        int n1 = s1.length(), n2 = s2.length();
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
+        return rec(n1-1,n2-1,s1,s2,dp);
     }
 };
