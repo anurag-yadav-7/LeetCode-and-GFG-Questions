e08f5b005d3cf3f37d5b5fc595b00addc4a2f742
Anurag Yadav <92670570+anurag-yadav-7@users.noreply.github.com>
2023-08-20 20:31:59 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,53 +10,43 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	int count(int arr[], int n, int x) {
-	    // code here
+	int count(int nums[], int n, int x) {
+	    int low = 0;
+	    int high = n;
+	    int first = -1;
+	    while(low <= high)
+	    {
+	        int mid = low + (high-low)/2;
+	        if(nums[mid] == x)
+	        {
+	            first = mid;
+	            low = mid + 1;
+	        }
+	        else if(nums[mid] > x)
+	            high = mid - 1;
+	        else
+	            low = mid + 1;
+	    }
 	    
-	       int low = 0, high = n-1;
-    int mid, first = -1, last = -1;
-    
-    
-    while(low <= high)
-    {
-        mid = low + (high - low) / 2;
-        
-        if(arr[mid] >= x)
-        {
-            if(arr[mid] == x)
-                first = mid;
-            high = mid - 1;
-        }
-        else
-        {
-            low = mid + 1;
-        }
-    }
-    
-    if(first == -1)
-        return 0;
-    
-    low = 0, high = n - 1 ;
-    while(low <= high)
-    {
-        mid = low + (high - low) / 2;
-        
-        if(arr[mid] > x)
-        {
-            high = mid - 1;
-        }
-        else
-        {
-            if(arr[mid] == x)
-                last = mid;
-            low = mid + 1;
-        }
-    }
-    
-    
-    
-    return last - first + 1;
-	    
+	    low = 0 ;
+	    high = n;
+	    int second = -1;
+	    while( low <= high)
+	    {
+	        int mid = low + (high-low)/2;
+	        if(nums[mid] == x)
+	        {
+	            second = mid;
+	            high = mid - 1;
+	        }
+	        else if(nums[mid] > x)
+	            high = mid - 1;
+	        else
+	            low = mid + 1;
+	    }
+	    if(first == -1)
+	        return 0;
+	    return first - second + 1;
 	}
 };
 
